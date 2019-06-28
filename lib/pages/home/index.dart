62668c54edc73cc8cecc1194fd8fd7c87e044967
Feng Yifei <fengyfei@gmail.com>
2019-06-28 17:08:38 +0800

    home banner

--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Home Page View',
-          ),
-        ],
+    final List<String> images = [
+      'https://images.pexels.com/photos/35857/amazing-beautiful-breathtaking-clouds.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      'https://images.unsplash.com/photo-1507608616759-54f48f0af0ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+      'https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    ];
+
+    return Container(
+      child: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(8.0),
+              height: 240,
+              child: Swiper(
+                viewportFraction: 0.8,
+                scale: 0.9,
+                autoplay: true,
+                autoplayDelay: 4000,
+                itemBuilder: (BuildContext context,int index){
+                  return Container(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: FadeInImage(
+                        image: NetworkImage(images[index]),
+                        fit: BoxFit.cover,
+                        placeholder: AssetImage('assets/images/loading.gif'),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: images.length,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
